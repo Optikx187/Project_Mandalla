b82f676fb4bce31a533a83f7f067de84ff00c8cd
Optikx187 <optik@Eluktronics.home>
2020-06-03 03:18:05 -0400

    formatting

--- Azure_TF/VNET_NSG_LNX/main.tf
+++ Azure_TF/VNET_NSG_LNX/main.tf
@@ -1,6 +1,5 @@
 provider "azurerm" {
   # Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider being u$  version = "=2.5.0"
-
   client_id       =var.client_id
   client_secret   =var.client_secret
   subscription_id =var.subscription_id
