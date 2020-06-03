4f7ba052c80d32a92971fd39ae5c204c1fb86943
Optikx187 <optik@Eluktronics.home>
2020-06-03 03:21:38 -0400

    add

---
+++ Azure_TF/VNET_NSG_LNX/connectionvars.tf
@@ -0,0 +1,17 @@
+variable "client_id"{
+        type = string
+        description = "Azure client ID"
+}
+variable "client_secret"{
+        type = string
+        description = "Azure client secret"
+}
+variable "subscription_id" {
+        type = string
+        description = "Azure subscription  ID"
+}
+
+variable "tenant_id"{
+        type = string
+        description = "Azure subscription  ID"
+}
