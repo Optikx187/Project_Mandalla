ea2ec4551259056000c4f8da1f4783eb70c86739
Optikx187 <optik@Eluktronics.home>
2020-06-08 00:19:16 -0400

    uupdated output. need to refine

--- Azure_TF/Ansible_Lab_Az_Terraform/output.tf
+++ Azure_TF/Ansible_Lab_Az_Terraform/output.tf
@@ -1,9 +1,3 @@
-output "lnx_vm0_ip" {
-   value = "${azurerm_public_ip.az-net-publicip0}"
-}
-output "win_vm0_ip" {
-   value = "${azurerm_public_ip.az-net-publicip1}"
-}
 output "Ansible_Host_Info" {
    value = "${azurerm_linux_virtual_machine.lnx-vm0}"
 }
