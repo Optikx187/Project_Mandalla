5aa6017107ae4258b9e76f2255b5b80cfc45adfc
Optikx187 <optik@Eluktronics.home>
2020-06-16 14:12:51 -0400

    updated information

--- AWS Packer/Example/setup.sh
+++ AWS Packer/Example/setup.sh
@@ -1,14 +1,7 @@
-#!/bin/bash
-infile=$1
-if [ -z "$infile" ]
-then
-        echo "Param \$infile not passed correctly"
-else
- echo "setting AWS connection vars"
- export AWS_MAX_ATTEMPTS=60
- export AWS_POLL_DELAY_SECONDS=60
- export AWS_ACCESS_KEY_ID=<key>
- export AWS_SECRET_ACCESS_KEY=<key>
- echo "Running packer build on $infile"
- packer build $infile
-fi
+!/bin/bash
+sudo apt-get -y update
+sudo apt-get -y install nano
+sudo apt-get -y install unzip
+#sudo apt-get -y install openssh-server 
+#sudo apt-get -y install openssh-clients
+sudo apt-get -y install wget
--- AWS Packer/Example/start-packer.sh
+++ AWS Packer/Example/start-packer.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+infile=$1
+if [ -z "$infile" ]
+then
+        echo "Param \$infile not passed correctly"
+else
+ echo "setting AWS connection vars"
  export AWS_MAX_ATTEMPTS=60
  export AWS_POLL_DELAY_SECONDS=60
- export AWS_ACCESS_KEY_ID=
- export AWS_SECRET_ACCESS_KEY=
-
- packer build ubuntu.json
+ export AWS_ACCESS_KEY_ID=<Key>
+ export AWS_SECRET_ACCESS_KEY=<Key>
+ echo "Running packer build on $infile"
+ packer build $infile
+fi
