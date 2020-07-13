f98dbd1afaa5b2f38cc4a71c0b05d8c81e713f58
Optikx187 <optik@Eluktronics.home>
2020-07-13 01:07:31 -0400

    added updates

--- Azure TF/Ansible Lab Azure Terraform/ansible.lnx.bootstrap.sh
+++ Azure TF/Ansible Lab Azure Terraform/ansible.lnx.bootstrap.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
-yum -y update
-yum -y install nano
-yum -y install unzip
-yum –y install openssh-server openssh-clients
-yum -y install wget
-yum -y install python3
-yum -y install python3-pip
-yum -y install epel-release
-yum -y install ansible
-yum -y install python-winrm
 
+sudo yum -y update --nobest
+sudo yum -y install nano --nobest
+sudo yum -y install unzip --nobest
+sudo yum -y install wget --nobest
+sudo yum -y install openssh-server openssh-clients --nobest
+sudo yum -y install python2  --nobest
+#ansible install
+sudo yum -y install python3 --nobest
+sudo yum -y install python3-pip --nobest
+sudo yum -y install epel-release --nobest
+sudo yum -y install ansible --nobest
+
+#install windows ansible dependency
+pip3 install pywinrm
+
+#terraform install
+sudo wget https://releases.hashicorp.com/terraform/0.12.26/terraform_0.12.26_linux_amd64.zip
+sudo unzip ./terraform_0.12.26_linux_amd64.zip -d /usr/local/bin/
+sudo rm ./terraform_0.12.26_linux_amd64.zip
