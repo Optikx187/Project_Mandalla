da3883d31c28cf38dcb50d7e0246046c58fe3a71
Optikx187 <Optikx187@outlook.com>
2021-11-24 19:58:08 -0500

    troubleshooting

--- AWS TF/Ansible_Lab_AWS_Terraform/dev/environments/devlab/security-groups.tf
+++ AWS TF/Ansible_Lab_AWS_Terraform/dev/environments/devlab/security-groups.tf
@@ -1,7 +1,7 @@
 locals {
    # this should give you 
-   private_formatted_list = "${formatlist("SubnetId=%s, var.vpc_private_subnets")}"
-   db_formatted_list = "${formatlist("SubnetId=%s, var.vpc_db_subnets")}"
+   private_formatted_list = "${formatlist("SubnetId=%s", var.vpc_private_subnets)}"
+   db_formatted_list = "${formatlist("SubnetId=%s", var.vpc_db_subnets)}"
 
    # combine the formatted list of parameter together using join
    private_subnet_cidr = "${join(" ", locals.private_formatted_list)}"
