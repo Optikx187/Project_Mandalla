8380c209e16f47837efee0536d707db6300a082f
Optikx187 <Optikx187@outlook.com>
2021-11-24 20:09:28 -0500

    troubleshooting

--- AWS TF/Ansible_Lab_AWS_Terraform/dev/environments/devlab/main.tf
+++ AWS TF/Ansible_Lab_AWS_Terraform/dev/environments/devlab/main.tf
@@ -9,8 +9,8 @@ terraform {
 locals {
   region = "us-east-1"
   # this should give you 
-  private_formatted_list = "${formatlist("SubnetId=%s", var.vpc_private_subnets)}"
-  db_formatted_list = "${formatlist("SubnetId=%s", var.vpc_db_subnets)}"
+  private_formatted_list = "${formatlist("%s", var.vpc_private_subnets)}"
+  db_formatted_list = "${formatlist("%s", var.vpc_db_subnets)}"
 
   # combine the formatted list of parameter together using join
   private_subnet_cidr = "${join(" ", local.private_formatted_list)}"
