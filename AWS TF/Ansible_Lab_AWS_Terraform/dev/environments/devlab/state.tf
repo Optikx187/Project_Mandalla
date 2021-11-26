1f41b8bf9165352b2ad39cf51e5f0bd78f155479
Optikx187 <Optikx187@outlook.com>
2021-11-26 01:36:25 -0500

    update

--- AWS TF/Ansible_Lab_AWS_Terraform/dev/environments/devlab/state.tf
+++ AWS TF/Ansible_Lab_AWS_Terraform/dev/environments/devlab/state.tf
@@ -1,4 +1,25 @@
-#KMS Key and Alias
+################################################################################
+# .tfstate file config
+################################################################################
+#cannot use variables for bucket names
+#must be commented out on first run. S3 backend not established. #chicken before egg type deal
+#bucket name must match variable 
+    #terrarofm init
+    #terraform plan -out test.tfplan
+    #terraform apply "test.tfplan"
+    
+/*    
+terraform {
+ backend "s3" {
+   bucket         = "dev-lab-s3-bucket" #update me
+   key            = "state/terraform.tfstate"
+   region         = "us-east-1" #update me
+   encrypt        = true
+   kms_key_id     = "alias/terraform-bucket-key"
+   dynamodb_table = "terraform-state"
+ }
+}
+*/
 resource "aws_kms_key" "terraform-bucket-key" {
  description             = "This key is used to encrypt bucket objects"
  deletion_window_in_days = 10
